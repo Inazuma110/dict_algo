2a127ceb8aa2bed28328a10b6409e1d081b310d0
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-06-24 00:52:01 +0900

    fix; digit sum

--- algorithm/digit_sum/main.cpp
+++ algorithm/digit_sum/main.cpp
@@ -8,19 +8,25 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> p;
 
+// ll digitSum(int n)
+// {
+//   int tmp = log10(n);
+//   ll res = 0;
+//   for (int i = tmp; i > 0; i--)
+//   {
+//     res += n / (pow(10, i));
+//     n -= ((n / int((pow(10, i)))) * pow(10, i));
+//   }
+//   res += n;
+//   return res;
+// }
+
 ll digitSum(int n)
 {
-  int tmp = log10(n);
-  ll res = 0;
-  for (int i = tmp; i > 0; i--)
-  {
-    res += n / (pow(10, i));
-    n -= ((n / int((pow(10, i)))) * pow(10, i));
-  }
-  res += n;
-  return res;
+  if(n < 10) return n;
+  else return n % 10 + digitSum(n / 10);
+  
 }
-
 int main()
 {
   int n;
