c83217ae46807448a5e245bb078904df0f25446b
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-03-04 03:22:33 +0900

    unionfind tree to struct

--- algorithm/union_find/main.cpp
+++ algorithm/union_find/main.cpp
@@ -5,66 +5,74 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> p;
 
-// 要素に対応する値は自分の根。根は要素数の-1倍したものを代入する。
-// 根ならそのグループの要素数(負)が、子であれば親の番号が入る。初期値-1。
-vector<int> uni(10000, -1);
-
-// 頂点aの所属するグループを調べる。
-int root(int a)
-{
-  // uni[a]が負の値なら親はa自身
-  if (uni[a] < 0) return a;
-  // 正の値であれば、親ルートを調べ戻り値で根に直接つなぐ。
-  else {
-    uni[a] = root(uni[a]);
-    return uni[a];
-    // return uni[a] = root(uni[a]);
+struct UnionFind {
+  // 要素に対応する値は自分の根。根は要素数の-1倍したものを代入する。
+  // 根ならそのグループの要素数(負)が、子であれば親の番号が入る。初期値-1。
+  vector<int> uni;
+  UnionFind(){
+    uni = vector<int>(100100, -1);
   }
-}
 
-// 頂点aとbをつなぐ、もともと同じグループの場合falseを返す。
-bool connect(int a, int b)
-{
-  a = root(a);
-  b = root(b);
-  if (a == b) return false;
-
-  // aを大きなグループにしたいので、逆であれば入れ替える。
-  if (uni[a] > uni[b])
+  // 頂点aの所属するグループを調べる。
+  int root(int a)
   {
-    int hoge = a;
-    a = b;
-    b = hoge;
+    // uni[a]が負の値なら親はa自身
+    if (uni[a] < 0) return a;
+    // 正の値であれば、親ルートを調べ戻り値で根に直接つなぐ。
+    else {
+      uni[a] = root(uni[a]);
+      return uni[a];
+      // return uni[a] = root(uni[a]);
+    }
   }
 
-  //aとbを結合し、bをaの親とする。
-  uni[a] += uni[b];
-  uni[b] = a;
-  return true;
-}
+  // 頂点aとbをつなぐ、もともと同じグループの場合falseを返す。
+  bool connect(int a, int b)
+  {
+    a = root(a);
+    b = root(b);
+    if (a == b) return false;
 
-//  頂点a, bが同じグループか
-bool isConnect(int a, int b)
-{
-  return root(a) == root(b);
-}
+    // aを大きなグループにしたいので、逆であれば入れ替える。
+    if (uni[a] > uni[b])
+    {
+      int hoge = a;
+      a = b;
+      b = hoge;
+    }
 
-// 頂点aを含むグループの頂点数を調べる
-int size(int a)
-{
-  return -uni[root(a)];
-}
+    //aとbを結合し、bをaの親とする。
+    uni[a] += uni[b];
+    uni[b] = a;
+    return true;
+  }
+
+  //  頂点a, bが同じグループか
+  bool isConnect(int a, int b)
+  {
+    return root(a) == root(b);
+  }
+
+  // 頂点aを含むグループの頂点数を調べる
+  int size(int a)
+  {
+    return -uni[root(a)];
+  }
+
+};
 
 int main()
 {
-  connect(2,3);
-  cout << root(3) << endl;
-  cout << size(2) << endl;
-  cout << isConnect(2,3) << endl;
-  cout << connect(2, 5) << endl;
-  cout << size(2) << endl;
-  cout << root(5) << endl;
-  cout << size(5) << endl;
+  UnionFind uf;
+  uf.connect(2,3);
+
+  cout << uf.root(3) << endl;
+  cout << uf.size(2) << endl;
+  cout << uf.isConnect(2,3) << endl;
+  cout << uf.connect(2, 5) << endl;
+  cout << uf.size(2) << endl;
+  cout << uf.root(5) << endl;
+  cout << uf.size(5) << endl;
 
   return 0;
 }
