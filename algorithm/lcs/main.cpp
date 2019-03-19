50bfeb9011286f82ba2a5a8d5c9c326076fad038
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-03-20 07:58:48 +0900

    lcs

---
+++ algorithm/lcs/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#if __has_include("print.hpp")
+  #include "print.hpp"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef pair<int, int> p;
+
+vector<vector<int>> dp(2000, vector<int>(2000));
+
+int solve(string s, string t){
+  int n = int(s.size());
+  int m = int(t.size());
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if(s[i] == t[j]){
+        dp[i+1][j+1] = dp[i][j] + 1;
+      }else{
+        dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+      }
+    }
+  }
+  return dp[n][m];
+}
+
+int main(){
+  string s, t;
+  cin >> s >> t;
+  solve(s, t);
+}
