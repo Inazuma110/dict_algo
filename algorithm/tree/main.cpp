bb1f19565f2fc3b4e040f511d8592eb272af6019
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-05-25 12:34:36 +0900

    add tree

--- algorithm/tree/main.cpp
+++ algorithm/tree/main.cpp
@@ -14,11 +14,42 @@ typedef pair<int, int> p;
 
 struct Tree {
   int node_num;
-  int edgeNum;
+  int edge_num;
+  int diameter = -1;
+  int leaf_node;
+  int far_index = -1;
   vector<vector<int>> cost;
+  vector<bool> visited;
+
+  Tree(int nn) : node_num(nn){
+    edge_num = node_num - 1;
+    cost = vector<vector<int>> (node_num);
+    visited = vector<bool> (node_num, false);
+  }
+
+
+  pair<int, int> get_far(int from, int d){
+    visited[from] = true;
+    for(auto a : cost[from]){
+      if(!visited[a]){
+        get_far(a, d+1);
+      }
+    }
+    if(diameter < d){
+      diameter = d;
+      far_index = from;
+    }
+    return {far_index, diameter};
+  }
+
+  int get_diameter(int from){
+    pair<int, int> i_and_d = get_far(0, 0);
+    init();
+    return diameter = get_far(i_and_d.first, 0).second;
+  }
 
   void init(){
-    return;
+    visited = vector<bool> (node_num, false);
   }
 
 };
@@ -28,12 +59,15 @@ int main(){
   cin.tie(0);
   int n;
   cin >> n;
-  Tree t;
-  t.edgeNum = n;
-  for (int i = 0; i < n; i++) {
+  Tree t(n);
+  for (int i = 0; i < n-1; i++) {
     int from, to;
     cin >> from >> to;
-
+    from--;
+    to--;
+    t.cost[from].push_back(to);
+    t.cost[to].push_back(from);
   }
-
+  t.get_diameter(0);
+  cout << t.diameter << endl;
 }
