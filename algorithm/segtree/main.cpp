58d3fd8bb95df6235324e777a152528b4f741f0a
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-07-09 17:25:42 +0900

    segtree test通した

--- algorithm/segtree/main.cpp
+++ algorithm/segtree/main.cpp
@@ -16,9 +16,9 @@ typedef pair<int, int> p;
 struct SegTree {
   int seg_tree_size = 1;
   int nums_size;
-  vector<int> data;
+  vector<ll> data;
 
-  SegTree(int origin_size, vector<int> a) {
+  SegTree(int origin_size, vector<ll> a) {
     // 簡単のため数列の要素数が2のべき乗と考える
     while(seg_tree_size < origin_size) seg_tree_size *= 2;
     nums_size = seg_tree_size;
@@ -26,10 +26,9 @@ struct SegTree {
 
     // 2分木の要素数は2*要素数-1
     // n + n/2 + n/4 + n/8 + ... 1 = 2*n-1
-    data = vector<int> (seg_tree_size, INT_MAX);
+    data = vector<ll> (seg_tree_size, pow(2, 31));
     for (int i = 0; i < origin_size; i++) {
       update(i, a[i]);
-      // data[i+seg_tree_size-nums_size] = a[i];
     }
   }
 
@@ -48,9 +47,11 @@ struct SegTree {
 
 
   // [a, b)の最小値を求める
-  // kは節点の番号。1, rはその節点が[1, r)に対応づいている
+  // kは節点の番号。l, rはその節点が[1, r)に対応づいている
   // (a, b, 0, 0, n)として呼び出す。
   int query(int a, int b, int k, int l, int r){
+    // cout << l << " : " << r << endl;
+    // cout << a << " : " << b << endl;
     // 交差しなければ
     if(r <= a || b <= l) return INT_MAX;
     // [a, b)が[1, r)を完全に含んでいれば、この節点の値
@@ -59,12 +60,13 @@ struct SegTree {
     else{
       int vl = query(a, b, k*2+1, l, (l+r)/2);
       int vr = query(a, b, k*2+2, (l+r)/2, r);
+      // cout << min(vl, vr) << endl;
       return min(vl, vr);
     }
   }
 
   int query(int a, int b){
-    return query(a, b, 0, 0, nums_size-1);
+    return query(a, b, 0, 0, nums_size);
   }
 
 };
@@ -73,10 +75,19 @@ struct SegTree {
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int n = 10;
-  vector<int> v = {1, 4, 6, 23, 5, 21, 9, 2, 3, 10};
+  int n, q;
+  cin >> n >> q;
+  vector<ll> v(n, INT_MAX);
   SegTree st(n, v);
-  cout << st.query(1, 4) << endl;
-  cout << st.query(3, 7) << endl;
-  cout << st.query(0, 1) << endl;
+  for (int i = 0; i < q; i++) {
+    int com, x, y;
+    cin >> com >> x >> y;
+    // print(st.data);
+    if(com == 0) {
+      st.update(x, y);
+    }
+    else{
+      cout << st.query(x, y+1) << endl;
+    }
+  }
 }
